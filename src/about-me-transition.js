05ae338cb834622b14b4a3c4eb87183e801e8a20
Roshan Bhatia <roshan@timebyping.com>
2023-06-02 23:09:30 -0700

    feat: autofetch about me section from github

--- src/about-me-transition.js
+++ src/about-me-transition.js
@@ -5,26 +5,63 @@ document.getElementById('about-link').addEventListener('click', function () {
     var footer = document.querySelector('footer');
     var logo = document.querySelector('.logo');
     var navigation = document.querySelector('.navigation');
-
+  
     mainSection.style.opacity = '0'; // fade out the main section
     mainSection.style.pointerEvents = 'none'; // disable pointer events
-
-    // Hide logo and navigation links with fade-out animation
+  
+    // Add fade-out class to logo and navigation
     logo.classList.add('fade-out');
     navigation.classList.add('fade-out');
-
+  
     // Hide all nav links
     for (var i = 0; i < navLinks.length; i++) {
-        navLinks[i].style.opacity = '0';
-        navLinks[i].style.pointerEvents = 'none'; // disable pointer events
+      navLinks[i].style.opacity = '0';
+      navLinks[i].style.pointerEvents = 'none'; // disable pointer events
     }
-
+  
     // Hide footer with fade-out animation
     footer.style.opacity = '0';
-
+  
+    // Fetch the Markdown file
+    fetch('https://raw.githubusercontent.com/roshbhatia/roshbhatia/main/README.md')
+      .then(response => response.text())
+      .then(markdown => {
+        // Extract the content within the <body> tags
+        var bodyContent = extractBodyContent(markdown);
+  
+        // Convert Markdown within the body to plain text
+        var plainText = markdownToPlainText(bodyContent);
+  
+        // Insert the plain text into the "About Me" section
+        aboutMe.innerText = plainText;
+  
+        // Add fade-in class to the "About Me" section
+        aboutMe.classList.remove('hidden');
+        aboutMe.classList.add('fade-in');
+      });
+  
     // Listen for transition end event
     mainSection.addEventListener('transitionend', function () {
-        aboutMe.classList.remove('hidden'); // make the about-me section visible
-        aboutMe.classList.add('fade-in'); // apply the fade-in animation
+      mainSection.style.display = 'none'; // hide the main section
+      footer.style.display = 'none'; // hide the footer
     }, { once: true });
-});
+  });
+  
+  // Function to extract content within the <body> tags
+  function extractBodyContent(markdown) {
+    var bodyStartIndex = markdown.indexOf('<body>') + '<body>'.length;
+    var bodyEndIndex = markdown.indexOf('</body>');
+    return markdown.slice(bodyStartIndex, bodyEndIndex);
+  }
+  
+  // Function to convert Markdown to plain text
+  function markdownToPlainText(markdown) {
+    // Remove Markdown syntax and tags using regular expressions
+    var plainText = markdown.replace(/[\*_~#\[\]!\-<>]/g, '');
+  
+    // Trim leading and trailing spaces
+    plainText = plainText.trim();
+  
+    return plainText;
+  }
+  
