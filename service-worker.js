893fc8f599be499efc368cff3c9bfc1a1d43d808
Roshan Bhatia <rshnbhatia@gmail.com>
2024-08-15 20:21:14 -0700

    wip

--- service-worker.js
+++ service-worker.js
@@ -11,8 +11,8 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
     const url = new URL(event.request.url);
 
-    // Rewrite the URL for the WASM file
-    if (url.pathname.endsWith('devenv.wasm')) {
+    // Check if the request URL matches the WASM URL
+    if (event.request.url === WASM_URL) {
         event.respondWith(
             fetch(WASM_URL, {
                 mode: 'cors',
