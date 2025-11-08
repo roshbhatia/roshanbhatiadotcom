c8d8eb3fcf744a26a20eba53ceb701326cbb1b26
Roshan Bhatia <rshnbhatia@gmail.com>
2025-11-08 15:59:39 -0800

    Simplify App to only HomePage for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,8 @@
-import { useState } from 'react'
-import Navigation from '@/components/Navigation.tsx'
+import React from 'react'
 import HomePage from '@/pages/HomePage.tsx'
-import DesignSystem from '@/pages/DesignSystem.tsx'
-import DemoPage from '@/pages/DemoPage.tsx'
 import './styles/globals.css'
 
 function AppContent() {
-  const [currentPage, setCurrentPage] = useState<'home' | 'design' | 'demo'>('home')
-
-  const renderPage = () => {
-    switch (currentPage) {
-      case 'design':
-        return <DesignSystem />
-      case 'demo':
-        return <DemoPage />
-      case 'home':
-      default:
-        return <HomePage />
-    }
-  }
-
   return (
     <div className="min-h-screen bg-bg text-text engineering-grid">
       <div className="sheet-border m-2 p-8">
@@ -28,7 +11,7 @@ function AppContent() {
             {/* Header Row */}
             <div className="col-span-12 grid grid-cols-12 gap-1 mb-4">
               <div className="col-span-8 cell-border p-3">
-                <Navigation currentPage={currentPage} onPageChange={(page) => setCurrentPage(page as 'home' | 'design' | 'demo')} />
+                <div className="mono text-xl text-left">roshanbhatia.com</div>
               </div>
               <div className="col-span-4 cell-border p-3">
                 <div className="text-xs text-border mono">SYSTEM STATUS: ONLINE</div>
@@ -41,7 +24,7 @@ function AppContent() {
               <div className="col-span-12 cell-border p-4">
                 <div className="grid grid-cols-12 gap-1">
                   <div className="col-span-12">
-                    {renderPage()}
+                    <HomePage />
                   </div>
                 </div>
               </div>
